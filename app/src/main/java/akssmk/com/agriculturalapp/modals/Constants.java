25695a47486388aaa1a76e05fd0d7205cf97602c
shivamkumard107 <shivam.kumard107@gmail.com>
2019-01-12 13:38:16 +0530

    added paytm feature to the app

---
+++ app/src/main/java/akssmk/com/agriculturalapp/modals/Constants.java
@@ -0,0 +1,12 @@
+package akssmk.com.agriculturalapp.modals;
+
+public class Constants {
+
+    public static final String M_ID = "IEEEDT67188777807734"; //Paytm Merchand Id we got it in paytm credentials
+    public static final String CHANNEL_ID = "WAP"; //Paytm Channel Id, got it in paytm credentials
+    public static final String INDUSTRY_TYPE_ID = "Retail"; //Paytm industry type got it in paytm credential
+
+    public static final String WEBSITE = "APP_STAGING";
+    public static final String CALLBACK_URL = "https://pguat.paytm.com/paytmchecksum/paytmCallback.jsp";
+
+}
